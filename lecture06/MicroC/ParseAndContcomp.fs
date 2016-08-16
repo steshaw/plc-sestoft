555c9f5d53239e4707354f8ef06f602006d820f5
Steven Shaw <steven@steshaw.org>
2016-08-16 16:49:37 +1000

    Import imp.zip and microc.zip

---
+++ lecture06/MicroC/ParseAndContcomp.fs
@@ -0,0 +1,7 @@
+(* File MicroC/ParseAndContcomp.fs *)
+
+let fromString = Parse.fromString
+
+let fromFile = Parse.fromFile
+
+let contCompileToFile = Contcomp.contCompileToFile
---
+++ lecture06/MicroC/ParseAndRun.fs
@@ -0,0 +1,7 @@
+(* File MicroC/ParseAndRun.fs *)
+
+let fromString = Parse.fromString
+
+let fromFile = Parse.fromFile
+
+let run = Interp.run
